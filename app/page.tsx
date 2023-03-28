0fb6d5c3c9826ee6fd645cdbbcc24acdfd4204be
Dom Sip <dominik.sipowicz@gmail.com>
2023-03-28 13:00:15 +0100

    preview comments

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>some change v2.2 hotfix fix</p>
+        <p>some change v2.3 testing comments</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
