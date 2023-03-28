9ae094ab8b307f2cf77b134d1f1ebe46880bf31f
Dom Sip <dominik.sipowicz@gmail.com>
2023-03-28 14:51:07 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>some change v3 in the works...</p>
+        <p>some change v3</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
